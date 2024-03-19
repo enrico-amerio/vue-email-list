a397a9a263251ebd391c57a3dbd810e6e40c5749
enrico-amerio <amerio.enrico95@gmail.com>
2024-03-19 14:06:11 +0100

    [add] Email generator

--- js/script.js
+++ js/script.js
@@ -3,6 +3,21 @@ const {createApp} = Vue;
 createApp({
   data(){
     return{
+      apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
+      email: []
+    }
+  },
+  methods:{
+    getApi(){
+      axios.get(this.apiUrl).then(res => {
+        this.email.push(res.data.response);
+      })
+    
+    }
+  },
+  mounted(){
+    for(let i = 0; i < 10; i++){
+      this.getApi();
     }
   }
 }).mount('#app');
